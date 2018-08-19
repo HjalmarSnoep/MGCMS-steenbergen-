ab626e0c93229467eaf14a8535eef4d2f2331c52
HjalmarSnoep <26629952+HjalmarSnoep@users.noreply.github.com>
2018-08-19 16:06:58 +0200

    Add files via upload

---
+++ cms_bugs/index.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: text/html; charset=utf-8'); // this is needed to get special characters right.
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<meta charset="utf-8">	
+	
+		<style>
+			body{	
+				font-family: sans-serif;
+			}
+		</style>
+	</head>
+<body>
+<h1>Kennisgeveing</h1>
+Het MGCMS Steenbergen is een product van Snoep Games / Making Games (MG) dat doorlopend wordt onderhouden.<br><BR>
+U kunt een vraag stellen of bug melden op <a target="_top" href="http://www.makinggames.org/nl/support/mgcms_steenbergen">www.makinggames.org/nl/support/mgcms_steenbergen</a>.<br>
+U verlaat dan de pagina om de vraag of bug te melden in de algemene Making Games omgeving.
+</body>
+</html>
